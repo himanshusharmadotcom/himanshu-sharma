97995642f7dbe01d9a480fb02dd57277f0f1cc26
Himanshu <hs7772043@gmail.com>
2025-01-01 20:59:40 +0530

    added files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js']
+    }
+  }
 })
